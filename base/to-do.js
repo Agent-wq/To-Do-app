6a08a9bba124dae4f99c5e50c0c5c4718d41d284
AadilShigan <adilshigan20@gmail.com>
2023-12-24 18:25:04 +0530

    Basically made the thing work

--- base/to-do.js
+++ base/to-do.js
@@ -18,5 +18,37 @@ function addTask(){
         description: taskDescription,
         completed: false
     };
+
+    // Add the tasks to the tasks array
+    tasks.push(task);
+
+    // Call the function that displays the tasks
+    displayTasks();
+
+    // Clear the form
+    document.getElementById("task-form").reset();
+
+    console.log("Task added");
+}
+
+// Display the tasks
+function displayTasks() {
     
+    // Get the task list container
+    const taskListContainer = document.getElementById("tasks-list");
+
+    // Clear the previous tasks
+    taskListContainer.innerHTML = '';
+
+    // Display each task in a list
+    tasks.forEach((tasks,index) => {
+        const taskItem = document.createElement("div");
+        taskItem.classList.add("task-item");
+
+        // Customize the display format as needed
+        taskItem.innerHTML = `<strong>${tasks.title}</strong><p>${tasks.description}</p>`;
+
+        // Append the task item to the task list container
+        taskListContainer.appendChild(taskItem);
+    });
 }
